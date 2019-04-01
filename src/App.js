8165e99f6ea051d2a6911f42a1ce1705df53d5f3
SahanBuddhika <sahan.bu.1994@gmail.com>
2019-04-01 20:17:44 +0530

    start to create pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router , Route , Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodoList from "./components/todos-list.component";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <div className = "container">
+          <h2>MERN-Stack Todo App</h2>
+        
+        <Route path="/" exact component={TodoList} />
+        <Route path="/edit/:id" exact component={EditTodo} />
+        <Route path="/create" exact component={CreateTodo} />
+
+        </div>
+      </Router>
+      );
   }
 }
 
---
+++ src/components/create-todo.component.js
@@ -0,0 +1,11 @@
+import React ,  {Component} from 'react';
+
+export default class CreateTodo extends Component{
+    render(){
+        return(
+            <div>
+                <p> Welcome Create Todo Component !</p>
+            </div>
+        )
+    }
+}
