8ed6fdfdb1445e0bb204fb99bc44ffa46afdcc08
SahanBuddhika <sahan.bu.1994@gmail.com>
2019-04-02 09:40:33 +0530

    add nav bar

--- src/App.js
+++ src/App.js
@@ -12,7 +12,26 @@ class App extends Component {
     return (
       <Router>
         <div className = "container">
-          <h2>MERN-Stack Todo App</h2>
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="https://codingthesmartway.com" target="-blank">
+          </a>
+       
+
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collapse nav-collapse">
+              <ul className="navbar-nav mr-auto">
+
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>  
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todos</Link>
+                </li> 
+                
+              </ul>
+            </div>
+        </nav>
         
         <Route path="/" exact component={TodoList} />
         <Route path="/edit/:id" exact component={EditTodo} />
