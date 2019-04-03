806eb5542cae0380586f0755a53180d8ac42ec84
SahanBuddhika <sahan.bu.1994@gmail.com>
2019-04-03 22:58:28 +0530

    establish mongodb db connection

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose')
 const PORT = 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
+mongoose.connect("mongodb+srv://sahan:Sa**1994@cluster0-7xoy9.mongodb.net/test?retryWrites=true",{ useNewUrlParser:true });
+const connection = mongoose.connection;
+
+connection.once('open',function(){
+    console.log("MongoDB database connection establish successfully");
+})
+
 app.listen(PORT,function(){
     console.log("Server is running on port:" + PORT );
 });
+
