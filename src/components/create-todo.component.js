ae8ef427825c439a1d06527b8da1c71262dd19be
SahanBuddhika <sahan.bu.1994@gmail.com>
2019-04-06 20:22:41 +0530

    start to eddi todos-list page

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -46,6 +46,13 @@ export default class CreateTodo extends Component{
         console.log(`Todo Priority: ${this.state.todo_priority}`);
         console.log(`Todo Completed: ${this.state.todo_completed}`);
 
+        const newTodo = {
+            todo_description:this.state.todo_description,
+            todo_responsible:this.state.todo_responsible,
+            todo_priority:this.state.todo_priority,
+            todo_completed:this.state.todo_completed
+        }
+
         axios.post('http://localhost:4000/todos/add',newTodo).then(res => console.log(res.data));
 
         this.setState({
--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,6 +1,24 @@
 import React ,  {Component} from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class TodoList extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {todos:[]};
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:4000/todos/')
+            .then(response => {
+                this.setState({todos:response.data});
+            })
+            .catch(function(err){
+                console.log(error);
+            })
+    }
+
     render(){
         return(
             <div>
